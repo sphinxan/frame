386f16226de3cf3e6c5d60515288c05f25d08e6b
) <dazaj.osamu@mail.ru>
2021-01-25 18:51:46 +0500

    frame

---
+++ frame/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace frame
+{
+    class Program
+    {
+        static void Main()
+        {
+            static void Main(string[] args)
+            {
+                WriteTextWithBorder("Menu:");
+                WriteTextWithBorder("");
+                WriteTextWithBorder(" ");
+                WriteTextWithBorder("Game Over!");
+                WriteTextWithBorder("Select level:");
+            }
+            static void WriteTextWithBorder(string text)
+            {
+                ToSymbols(text);
+
+                Console.Write("| ");
+                Console.Write(text);
+                Console.WriteLine(" |");
+
+                ToSymbols(text);
+            }
+
+            static void ToSymbols(string text)
+            {
+                Console.Write('+');
+                for (int i = 1; i < text.Length + 3; i++)
+                    Console.Write('-');
+                Console.WriteLine('+');
+            }
+        }
+    }
+}
